d30ea629a6b401c305643df54fe7dc486c4ce914
fizzydrinks <gabriel@lins.dev>
2023-12-15 01:24:35 -0300

    Add scrolls to inventory, improve print UI

--- src/components/CharacterSheetPreview.tsx
+++ src/components/CharacterSheetPreview.tsx
@@ -37,71 +37,91 @@ const CharacterSheetPreview: FC = () => {
   const statTotals = addStats(abilityScores, raceASI);
 
   const items = {
+    "": [],
     mace: ["Maça", "Escudo"],
     daggers: ["Adaga x2"],
     halberd: ["Alabarda"],
     bow: ["Arco", "Flecha x10"],
-    scrolls: ["Pergaminho x2"],
-  }[weaponChoice || "mace"];
+    scrolls: [],
+  }[weaponChoice || ""];
+
+  const scrollsCount =
+    [3, 2, 1, 0][magicalInclination || 0] + +(weaponChoice === "scrolls") * 2;
 
   return (
-    <div onClick={() => print()}>
-      <Heading>{charName}</Heading>
-      <Paragraph className="text-gray-300 text-bold">{raceName}</Paragraph>
-      <Paragraph>{charBackstory}</Paragraph>
-      <Heading className="text-xl">Pontos de habilidade</Heading>
-      <StatInput value={statTotals} readOnly />
-      <Heading className="text-xl">Modificadores</Heading>
-      <StatInput
-        value={{
-          str: statToMod(statTotals?.str),
-          dex: statToMod(statTotals?.dex),
-          con: statToMod(statTotals?.con),
-          int: statToMod(statTotals?.int),
-          wis: statToMod(statTotals?.wis),
-          cha: statToMod(statTotals?.cha),
-        }}
-        readOnly
-      />
-      <div className="gap-1 flex">
-        <label className="flex-col flex gap-1 items-center">
-          CA
-          <TextInput
-            readOnly
-            value={
-              10 + statToMod(statTotals?.dex) + +(weaponChoice === "mace") * 2
-            }
-            className="w-12 text-center"
-          />
-        </label>
-        <label className="flex-col flex gap-1 items-center">
-          Iniciativa
-          <TextInput
-            readOnly
-            value={statToMod(statTotals?.dex)}
-            className="w-12 text-center"
-          />
-        </label>
-        <label className="flex-col flex gap-1 items-center">
-          PV
-          <TextInput
-            readOnly
-            value={
-              statToMod(statTotals?.con) + (6 + (magicalInclination || 0) * 2)
-            }
-            className="w-12 text-center"
-          />
-        </label>
+    <>
+      <div className="print:hidden">
+        <Heading>Pré-visualização</Heading>
+        <button
+          className="border-2 font-bold rounded border-slate-300 p-2 my-2 print:hidden"
+          onClick={() => print()}
+        >
+          Imprimir ficha
+        </button>
       </div>
-      <Heading className="text-xl">Inventário</Heading>
-      <ul>
-        {items?.map((i) => (
-          <li key={i} className="list-disc ml-6">
-            {i}
-          </li>
-        ))}
-      </ul>
-    </div>
+      <div className="bg-slate-600 text-slate-300 rounded shadow-md px-3 py-1">
+        <Heading>{charName}</Heading>
+        <Paragraph className="text-gray-300 text-bold">{raceName}</Paragraph>
+        <Paragraph>{charBackstory}</Paragraph>
+        <Heading className="text-xl">Pontos de habilidade</Heading>
+        <StatInput value={statTotals} readOnly />
+        <Heading className="text-xl">Modificadores</Heading>
+        <StatInput
+          value={{
+            str: statToMod(statTotals?.str),
+            dex: statToMod(statTotals?.dex),
+            con: statToMod(statTotals?.con),
+            int: statToMod(statTotals?.int),
+            wis: statToMod(statTotals?.wis),
+            cha: statToMod(statTotals?.cha),
+          }}
+          readOnly
+        />
+        <div className="gap-1 flex">
+          <label className="flex-col flex gap-1 items-center">
+            CA
+            <TextInput
+              readOnly
+              value={
+                10 + statToMod(statTotals?.dex) + +(weaponChoice === "mace") * 2
+              }
+              className="w-12 text-center"
+            />
+          </label>
+          <label className="flex-col flex gap-1 items-center">
+            Iniciativa
+            <TextInput
+              readOnly
+              value={statToMod(statTotals?.dex)}
+              className="w-12 text-center"
+            />
+          </label>
+          <label className="flex-col flex gap-1 items-center">
+            PV
+            <TextInput
+              readOnly
+              value={
+                statToMod(statTotals?.con) + (6 + (magicalInclination || 0) * 2)
+              }
+              className="w-12 text-center"
+            />
+          </label>
+        </div>
+        <Heading className="text-xl">Inventário</Heading>
+        <ul>
+          {items?.map((i) => (
+            <li key={i} className="list-disc ml-6">
+              {i}
+            </li>
+          ))}
+          {scrollsCount > 0 && (
+            <li className="list-disc ml-6">
+              Pergaminho mágico x{scrollsCount}
+            </li>
+          )}
+        </ul>
+      </div>
+    </>
   );
 };
 
